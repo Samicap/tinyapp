0452c255270e146540f2e36abe21591575bae367
Samicap <lovestochasewindmills@gmail.com>
2020-12-08 01:37:02 +0000

    added body parser to express_server

--- express_server.js
+++ express_server.js
@@ -9,6 +9,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -17,6 +21,11 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 
 app.get("/urls/:shortURL", (req, res) => {
   // const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
