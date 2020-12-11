1421576a58094f2413089cd61ffd45a50636551a
Samicap <lovestochasewindmills@gmail.com>
2020-12-11 23:19:54 +0000

    added mocha and chai test to test getUserIdByEmail function

---
+++ test/helpersTest.js
@@ -0,0 +1,35 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedOutput = {
+      id: "userRandomID", 
+      email: "user@example.com", 
+      password: "purple-monkey-dinosaur"
+    };
+
+    assert.deepEqual(user, expectedOutput, 'expectedOutput is not a `user` object');
+  });
+
+  it('should return undefined if email is non-existant', function() {
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedOutput = undefined;
+    assert.equal(undefined, expectedOutput, 'expectedOutput is undefined');
+  });
+});
